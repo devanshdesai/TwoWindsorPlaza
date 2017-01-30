79e748ec83902d432cef831158a240f0bbdfdde2
Devansh Desai <devanshdesai@hotmail.com>
2017-01-30 15:43:49 -0500

    Changed email address for php file

--- sendemail.php
+++ sendemail.php
@@ -9,7 +9,7 @@ if (isset($_POST['submit']))
     $message = $_POST['message'];
 
     $from = "From: Two Windsor Plaza <leasing@twowindsorplaza.com>\r\n";
-	$to = "leasing@twowindsorplaza.com";
+	$to = "windsor2@ptd.net";
     $subject = "Contact Us Submission - Two Windsor Plaza";
     $body = "Here is the contact form submission information:\n\n\tName:\n\t$name\n\n\tE-Mail:\n\t$email\n\n\tPhone:\n\t$phone\n\n\tBusiness:\n\t$business\n\n\tMessage:\n\t$message";
 
